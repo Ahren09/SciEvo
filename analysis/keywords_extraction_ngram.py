93dd7c288db208a3a6f67a2e6e3de77d3fd9fc9f
Ahren_Jin <1099052927@qq.com>
2023-10-30 10:52:50 -0400

    Parallelized vectorizer

--- analysis/keywords_extraction_ngram.py
+++ analysis/keywords_extraction_ngram.py
@@ -1,40 +1,19 @@
 """
 Extract keywords from the titles / abstracts (summary) using n-gram.
 """
-
+import itertools
 import os
 import os.path as osp
 import pickle
-import re
 import time
-from functools import lru_cache
 
-from nltk import WordNetLemmatizer, word_tokenize
+from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from arguments import parse_args
 from utility.utils_data import load_data
 from utility.utils_misc import project_setup
-
-stop_set = set(stopwords.words())
-lemmatizer = WordNetLemmatizer()
-
-
-@lru_cache(maxsize=10000)
-def lemmatize(word):
-    return lemmatizer.lemmatize(word.lower().strip())
-
-
-def tokenize(text):
-    text = re.sub('[^a-zA-Z0-9]+', ' ', text)
-    words = [lemmatize(w) for w in word_tokenize(text)]
-    return [w for w in words if w not in stop_set]
-
-
-
-
-
 
 
 def get_vectorizer(args, data=None):
@@ -49,65 +28,68 @@ def get_vectorizer(args, data=None):
     """
     path = osp.join(args.output_dir, 'tfidf_vectorizer.pkl')
 
-    if False:  # osp.exists(path):
+    if osp.exists(path):
         with open(path, 'rb') as f:
             vectorizer = pickle.load(f)
             return vectorizer
 
     else:
-        if USE_STEMMER:
-
-            english_stemmer = Stemmer.Stemmer('en')
-
-            class StemmedTfidfVectorizer(TfidfVectorizer):
-                def build_analyzer(self):
-                    analyzer = super(TfidfVectorizer, self).build_analyzer()
-                    return lambda doc: english_stemmer.stemWords(analyzer(doc))
-
-            vectorizer = StemmedTfidfVectorizer(tokenizer=tokenize, min_df=5, stop_words='english', analyzer='word',
-                                           ngram_range=(
-                                               1, 4))
-
-        else:
-
-
-            # Initialize a CountVectorizer/TfidfVectorizer with ngram_range=(1, 4)
-
-            # vectorizer = CountVectorizer(ngram_range=(1, 4), min_df=5)
-            vectorizer = TfidfVectorizer(tokenizer=tokenize, ngram_range=(1, 4), min_df=5, stop_words='english')
 
         abstracts = [abs for abs in data['summary'].tolist() if isinstance(abs, str)]
 
-        t0 = time.time()
-        # Fit and transform the summaries to get feature matrix
-        vectorizer.fit_transform(abstracts)
-        print(f"Time taken to fit and transform the summaries: {time.time() - t0:.2f} seconds")
+        if args.n_procs == 1:
+            # Initialize a CountVectorizer/TfidfVectorizer with ngram_range=(1, 4)
 
-        with open(path, 'wb') as f:
-            pickle.dump(vectorizer, f)
+            # vectorizer = CountVectorizer(ngram_range=(1, 4), min_df=5)
+            vectorizer = TfidfVectorizer(ngram_range=(1, 4), min_df=5, stop_words='english')
+
+            t0 = time.time()
+            # Fit and transform the summaries to get feature matrix
+            vectorizer.fit_transform(abstracts)
+            print(f"Time taken to fit and transform the summaries: {time.time() - t0:.2f} seconds")
+
+            with open(path, 'wb') as f:
+                pickle.dump(vectorizer, f)
+
+
+        else:
+
+            from model.vectorizer import CustomTfidfVectorizer
+            vectorizer = CustomTfidfVectorizer(args)
+            dtm = vectorizer.fit_transform(abstracts, range(1, 5), stopwords, num_threads=args.n_procs)
+            print("\nDTM from TF-IDF Vectorizer:")
+            print(dtm.toarray())
+
+            vectorizer.save()
+
+
+
+            #
+            # # Extract n-grams of length 1 to 4 in parallel
+            # n_grams_list = extract_ngrams_parallel(abstracts, range(1, 5), stopwords, num_threads=args.n_procs)
+            #
+            # # Filter n-grams by minimum document frequency
+            # n_grams_list = filter_ngrams_by_min_df(n_grams_list, 5)
+            #
+            # # Build CountVectorizer-like matrix
+            # dtm = build_count_vectorizer_matrix(n_grams_list)
+
+
 
     return vectorizer
 
 
 def main():
-    df = load_data(args)
+    df = load_data(args, subset="last_10000")
     vectorizer = get_vectorizer(args, data=df)
 
     # Get feature names (i.e., the n-grams)
     feature_names = vectorizer.get_feature_names_out()
 
-    # Sum up the counts of each n-gram across all documents
-    # Sort n-grams and counts by frequency
-    sorted_indices = ngram_counts.argsort()[::-1]
-    sorted_ngrams = [(feature_names[i], ngram_counts[i]) for i in sorted_indices]
-
 
 if __name__ == "__main__":
     project_setup()
     args = parse_args()
-
-    import Stemmer
-    USE_STEMMER = True
-
-    os.makedirs(args.output_dir, exist_ok=True)
+    stopwords = set(stopwords.words("english"))
     main()
+
---
+++ model/vectorizer.py
@@ -0,0 +1,155 @@
+import itertools
+import os
+import os.path as osp
+import pickle
+import time
+from collections import Counter
+from concurrent.futures import ThreadPoolExecutor
+
+import scipy.sparse as sp
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
+def preprocess_text(text):
+    """
+    Lowercases the text.
+    """
+    return text.lower()
+
+
+def should_keep_ngram(n_gram, stop_set):
+    """
+    Checks if all words in an n-gram are stop words.
+    """
+    return not all(word in stop_set for word in n_gram.split())
+
+
+def extract_ngrams_from_text(text, n, stop_set):
+    """
+    Extracts n-grams from a single document.
+    """
+    tokens = text.split()
+    n_grams = zip(*[tokens[i:] for i in range(n)])
+    return [" ".join(n_gram) for n_gram in n_grams if should_keep_ngram(" ".join(n_gram), stop_set)]
+
+
+def extract_ngrams_parallel(documents, n_range, stop_set, num_threads=4):
+    """
+    Parallelizes the extraction of n-grams from multiple documents.
+    """
+    n_grams_list = []
+    for n in n_range:
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            n_grams_for_n = list(
+                executor.map(lambda doc: extract_ngrams_from_text(preprocess_text(doc), n, stop_set), documents))
+        n_grams_list.append(n_grams_for_n)
+
+    # Combine the n-grams of different lengths for each document
+    combined_n_grams_list = [list(itertools.chain.from_iterable(doc_n_grams)) for doc_n_grams in zip(*n_grams_list)]
+    return combined_n_grams_list
+
+
+def filter_ngrams_by_min_df(n_grams_list, min_df=5):
+    """
+    Filters out n-grams that appear less than `min_df` times across all documents.
+    """
+    global_counter = Counter(itertools.chain.from_iterable(n_grams_list))
+    return [[n_gram for n_gram in doc if global_counter[n_gram] >= min_df] for doc in n_grams_list]
+
+
+def build_count_vectorizer_matrix(n_grams_list):
+    """
+    Constructs a CountVectorizer-like matrix from the list of n-grams for each document.
+    """
+    counters = [Counter(doc_n_grams) for doc_n_grams in n_grams_list]
+    vocabulary = {}
+    for counter in counters:
+        for n_gram in counter.keys():
+            if n_gram not in vocabulary:
+                vocabulary[n_gram] = len(vocabulary)
+
+    num_docs = len(counters)
+    num_terms = len(vocabulary)
+    indptr = [0]
+    indices = []
+    data = []
+    for counter in counters:
+        for term, freq in counter.items():
+            index = vocabulary[term]
+            indices.append(index)
+            data.append(freq)
+        indptr.append(len(indices))
+
+    return sp.csr_matrix((data, indices, indptr), shape=(num_docs, num_terms), dtype=int)
+
+
+class BaseVectorizer:
+    def __init__(self, args):
+        self.n_grams_list = None
+        self.dtm = None
+        self.args = args
+
+    def build_dtm(self, n_grams_list):
+        counters = [Counter(doc_n_grams) for doc_n_grams in n_grams_list]
+        vocabulary = {}
+        for counter in counters:
+            for n_gram in counter.keys():
+                if n_gram not in vocabulary:
+                    vocabulary[n_gram] = len(vocabulary)
+
+        num_docs = len(counters)
+        num_terms = len(vocabulary)
+        indptr = [0]
+        indices = []
+        data = []
+        for counter in counters:
+            for term, freq in counter.items():
+                index = vocabulary[term]
+                indices.append(index)
+                data.append(freq)
+            indptr.append(len(indices))
+
+        self.dtm = sp.csr_matrix((data, indices, indptr), shape=(num_docs, num_terms), dtype=int)
+
+    def save(self, path=None):
+
+        path = osp.join(self.args.output_dir, self.__class__.__name__) if path is None else path
+        print(f"Saving vectorizer to {path}...")
+        os.makedirs(path, exist_ok=True)
+
+        sp.save_npz(osp.join(path, 'csr_matrix.npz'), self.dtm)
+        with open(osp.join(path, "n_grams_list.pkl"), 'wb') as f:
+            pickle.dump(self.n_grams_list, f)
+
+    def load(self, path):
+        path = osp.join(self.args.output_dir, self.__class__.__name__) if path is None else path
+        print(f"Loading vectorizer from {path}...")
+        self.dtm = sp.load_npz(osp.join(path, 'csr_matrix.npz'))
+        with open(osp.join(path, "n_grams_list.pkl"), 'rb') as f:
+            self.n_grams_list = pickle.load(f)
+
+
+class CustomCountVectorizer(BaseVectorizer):
+    def fit_transform(self, documents, n_range, stop_set, num_threads=4):
+        t0 = time.time()
+        self.n_grams_list = extract_ngrams_parallel(documents, n_range, stop_set, num_threads)
+        self.n_grams_list = filter_ngrams_by_min_df(self.n_grams_list, 5)
+        self.build_dtm(self.n_grams_list)
+        print(f"Fit and transform takes {time.time() - t0:.2f} secs")
+        return self.dtm
+
+
+class CustomTfidfVectorizer(BaseVectorizer):
+    def fit_transform(self, documents, n_range, stop_set, num_threads=4):
+        t0 = time.time()
+        self.n_grams_list = extract_ngrams_parallel(documents, n_range, stop_set, num_threads)
+        self.n_grams_list = filter_ngrams_by_min_df(self.n_grams_list, 5)
+        self.build_dtm(self.n_grams_list)
+
+        # Apply TF-IDF transformation
+        transformer = TfidfTransformer()
+        self.dtm = transformer.fit_transform(self.dtm)
+
+        print(f"Fit and transform takes {time.time() - t0:.2f} secs")
+
+        return self.dtm
