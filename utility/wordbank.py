f0aa10e9e6c84e595e2eeb8b65874d75c42503ea
Ahren_Jin <1099052927@qq.com>
2023-11-05 17:20:44 -0500

    Create wordbank.py

---
+++ utility/wordbank.py
@@ -0,0 +1,29 @@
+PRONOUNS = ['he', 'her', 'hers', 'herself', 'him', 'himself', 'his', 'i', 'it', 'its', 'itself', 'me', 'mine', 'my',
+            'myself', 'our', 'ours', 'ourselves', 'she', 'that', 'their', 'theirs', 'them', 'themselves', 'these',
+            'they', 'this', 'those', 'us', 'we', 'what', 'which', 'who', 'whom', 'you', 'your', 'yours', 'yourself',
+            'yourselves']
+
+CONJUNCTIONS = ['after', 'although', 'and', 'as', 'as if', 'as long as', 'as much as', 'as soon as', 'as though',
+                'because', 'before', 'both', 'but', 'either', 'even', 'even if', 'even though', 'for', 'if', 'if only',
+                'if then', 'if when', 'inasmuch', 'just in case', 'lest', 'neither', 'nor', 'now', 'now since',
+                'now that', 'now when', 'once', 'or', 'provided', 'provided that', 'rather', 'since', 'so', 'so that',
+                'supposing', 'that', 'though', 'till', 'unless', 'until', 'when', 'whenever', 'where', 'wherever',
+                'whether', 'while', 'yet']
+
+PREPOSITIONS = ['aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'amid', 'among', 'anti', 'around',
+                'as', 'at', 'before', 'behind', 'below', 'beneath', 'beside', 'besides', 'between', 'beyond', 'but',
+                'by', 'concerning', 'considering', 'despite', 'down', 'during', 'except', 'excepting', 'excluding',
+                'following', 'for', 'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'of', 'off', 'on', 'onto',
+                'opposite', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'than',
+                'through', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'versus',
+                'via', 'with', 'within', 'without']
+
+MEANINGLESS_NOUNS = [
+    "model", "study", "this"
+]
+MEANINGLESS_ADJECTIVES = ['able', 'bad', 'best', 'better', 'big', 'capable', 'different', 'early', 'few', 'first',
+                          'good',
+                          'great',
+                          'high', 'important', 'large', 'late', 'likely', 'long', 'main', 'major', 'many', 'more',
+                          'new', 'next', 'novel', 'old', 'other', 'poor', 'possible', 'previous', 'recent', 'same',
+                          'short', 'small', 'such', 'worst', 'young']
