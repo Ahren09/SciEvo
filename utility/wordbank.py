e32689ed983f68275d0871103ad4a61359acf779
Ahren_Jin <1099052927@qq.com>
2023-11-05 17:20:44 -0500

    Create wordbank.py

---
+++ utility/wordbank.py
@@ -0,0 +1,36 @@
+PRONOUNS = ['he', 'her', 'hers', 'herself', 'him', 'himself', 'his', 'i', 'it', 'its', 'itself', 'me', 'mine', 'my',
+            'myself', 'our', 'ours', 'ourselves', 'she', 'that', 'their', 'theirs', 'them', 'themselves', 'these',
+            'they', 'this', 'those', 'us', 'we', 'what', 'which', 'who', 'whom', 'you', 'your', 'yours', 'yourself',
+            'yourselves']
+
+CONJUNCTIONS = ['after', 'although', 'and', 'as', 'as if', 'as long as', 'as much as', 'as soon as', 'as though',
+                'because', 'before', 'both', 'but', 'either', 'even', 'even if', 'even though', 'for', 'if', 'if only',
+                'if then', 'if when', 'inasmuch', 'just in case', 'lest', 'neither', 'nor', 'now', 'now since',
+                'now that', 'now when', 'once', 'or', 'provided', 'provided that', 'rather', 'since', 'so', 'so that',
+                'supposing', 'that', 'though', 'till', 'unless', 'until', 'when', 'whenever', 'where', 'wherever',
+                'whether', 'while', 'yet']
+
+PREPOSITIONS = ['aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'amid', 'among', 'anti', 'around',
+                'as', 'at', 'before', 'behind', 'below', 'beneath', 'beside', 'besides', 'between', 'beyond', 'but',
+                'by', 'concerning', 'considering', 'despite', 'down', 'during', 'except', 'excepting', 'excluding',
+                'following', 'for', 'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'of', 'off', 'on', 'onto',
+                'opposite', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'than',
+                'through', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'versus',
+                'via', 'with', 'within', 'without']
+
+MEANINGLESS_NOUNS = ['algorithm', 'analysis', 'application', 'approach', 'experiment', 'framework', 'method', 'model', 'paper', 'performance', 'problem', 'research', 'result', 'study', 'task', 'technique']
+
+
+MEANINGLESS_ADJECTIVES = ['able', 'bad', 'best', 'better', 'big', 'capable', 'different', 'early', 'few', 'first',
+                          'good',
+                          'great',
+                          'high', 'important', 'large', 'late', 'likely', 'long', 'main', 'major', 'many', 'more',
+                          'new', 'next', 'novel', 'old', 'outstanding', 'other', 'poor', 'possible', 'previous',
+                          'recent',
+                          'same',
+                          'short', 'significant', 'small', 'such', 'worst', 'young']
+
+
+MEANINGLESS_VERBS = ['be', 'become', 'begin', 'bring', 'build', 'buy', 'call', 'can', 'come', 'could', 'demonstrate',
+                     'do',  'feel',
+                     'find', 'get', 'give', 'go', 'have', 'hear', 'help', 'keep', 'know', 'leave', 'let', 'like', 'live', 'look', 'make', 'may', 'mean', 'might', 'move', 'need', 'play', 'put', 'run', 'say', 'see', 'seem', 'should', 'show', 'start', 'take', 'talk', 'tell', 'think', 'try', 'turn', 'use', 'want', 'will', 'would', 'write']
