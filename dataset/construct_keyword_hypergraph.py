9ef3d59a813c4226b9e7497950688b11a17519c0
Ahren09 <yjin328@gatech.edu>
2024-07-04 15:55:11 -0700

    Update scripts for updating hypergraph

--- dataset/construct_keyword_hypergraph.py
+++ dataset/construct_keyword_hypergraph.py
@@ -81,15 +81,18 @@ if __name__ == "__main__":
             edge_df = pd.read_parquet(path_graph)
         elif args.graph_backend == "rapids":
             import cudf
-            edge_df = cudf.read_parquet(path_graph)
+            edge_df = cudf.read_parquet(path_graph).astype({
+                'source': 'str',
+                'destination': 'str',
+                'index_paper': 'int32',
+                'published_year': 'int32',
+                'published_month': 'int32' 
+            })
         
     
     print(f"Building the graph ...", end=" ")
     t0 = time()
     
-    # Construct snapshot for each year
-    
-    # Creating a Graph using cuGraph
 
     if args.graph_backend == "networkx":
         G = nx.MultiGraph()
@@ -97,16 +100,101 @@ if __name__ == "__main__":
 
     elif args.graph_backend == "rapids":
         import cugraph
-        G = cugraph.MultiGraph()
+        
+        
+        stats_d = []
+        
+        # Construct snapshot for each year
+        for year in range(1985, 2025):
+            G = cugraph.MultiGraph()
+            
+            edges = edge_df.query(f"published_year == {year}")
+            
+            if len(edges) == 0:
+                continue
+            # G.from_cudf_edgelist(edge_df, source='source', destination='destination', edge_attr='index_paper', renumber=True)
+            G.from_cudf_edgelist(edges, source='source', destination='destination', edge_attr='index_paper', renumber=True)
+            
+            # Get node mappings
+            nodes_mapping = G.nodes().to_pandas().to_list()
+            node_index_to_id = {node_id: node_index for node_index, node_id in enumerate(nodes_mapping)}
+            custom_map = G.nodes().to_frame(name="keyword")
+            # Add an 'id' column
+            custom_map['node_id'] = cudf.RangeIndex(start=0, stop=len(custom_map))
+            
+            # Extract edge list from the existing graph
+            edge_list = G.view_edge_list()
+            # Merge custom mapping to apply it
+            edge_list = edge_list.merge(custom_map, left_on='source', right_on='keyword', how='left')\
+                                .drop(columns=['source', 'keyword'])\
+                                .rename(columns={'node_id': 'source'})
+            edge_list = edge_list.merge(custom_map, left_on='destination', right_on='keyword', how='left')\
+                                .drop(columns=['destination', 'keyword'])\
+                                .rename(columns={'node_id': 'destination'})
 
-        # G.from_cudf_edgelist(edge_df, source='source', destination='destination', edge_attr='index_paper', renumber=True)
-        G.from_cudf_edgelist(edge_df.query("published_year == 2024"), source='source', destination='destination', edge_attr='index_paper', renumber=True)
-        print(f"Building the graph: {time() - t0:.2f} seconds")
+            edge_list = edge_list.astype({'source': 'int32', 'destination': 'int32', 'index_paper': 'int32'})
 
-        # Degree calculation considering multiple edges
-        degrees = G.degrees()
-        print("Degree of each vertex (keyword), considering multiple edges:")
-        print(degrees)
+            # Recreate the graph as a multigraph
+            multi_G = cugraph.MultiGraph()
+            multi_G.from_cudf_edgelist(edge_list, source='source', destination='destination', renumber=False)
+            
+            # Create a simple graph with 'weight' attribute as the number of edges between two nodes
+            simple_G = cugraph.Graph()
+            edge_list['weight'] = 1
+            
+            simple_edge_list = edge_list[['source', 'destination', 'weight']].groupby(['source', 'destination'], as_index=False).sum()
+            simple_G.from_cudf_edgelist(simple_edge_list, source='source', destination='destination', edge_attr='weight', renumber=False)
+
+     
+            components_df = cugraph.connected_components(simple_G)
+            
+            num_components = components_df['labels'].nunique()
+            
+            grouped = components_df.groupby('labels')['vertex'].agg(list)
+            
+            component_sizes = grouped.to_pandas().apply(len).sort_values(ascending=False)
+            
+            max_component_size = component_sizes.max()
+            
+            print(f"[Year={year}]: #Nodes={simple_G.number_of_nodes()}, #Edges={simple_G.number_of_edges()}, #CC={num_components} (Max: {component_sizes.iloc[0]}, 2nd Max: {component_sizes.iloc[1] if len(component_sizes) > 1 else 0}")
+            
+            stats_d += [{
+                "Number of Nodes": simple_G.number_of_nodes(),
+                "Number of Edges": simple_G.number_of_edges(),
+                "Number of Components": num_components,
+                "Max Component Size": component_sizes.iloc[0],
+                "Second Max Component Size": component_sizes.iloc[1] if len(component_sizes) > 1 else 0,
+                "Year": year,
+                
+            }]
+            
+            continue
+
+
+            k = 3
+            core_df = cugraph.k_core(G, k)
+            
+            # Calculate average degree
+            degree_df = G.degrees()
+            average_degree = degree_df['degree']
+            
+            component_sizes = labels.value_counts().sort_values(ascending=False)
+            largest_component_size = component_sizes.iloc[0]
+            
+
+            # Degree calculation considering multiple edges
+            degrees = G.degrees()
+            print("Degree of each vertex (keyword), considering multiple edges:")
+            print(degrees)
+            
+    
+    stats_df = pd.DataFrame(stats_d).set_index("Year")
+    
+    stats_df.to_excel(osp.join(args.output_dir, "stats_nodes_in_connected_components.xlsx"))
+
+    # Use the Louvain algorithm to find communities
+    df_louvain, modularity_score = cugraph.louvain(G)
+    print("Modularity of partitioning: ", modularity_score)
 
     # Detect communities
     from networkx.algorithms.community import greedy_modularity_communities
